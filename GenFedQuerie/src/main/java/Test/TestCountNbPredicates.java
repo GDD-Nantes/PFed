b1753bd6aefad0ecf193ac2ef99617ab106fd56f
HacquesF <florian.hacques@etu.univ-nantes.fr>
2018-12-03 23:52:35 +0100

    Added Test to count predicates from Querie logs

---
+++ GenFedQuerie/src/main/java/Test/TestCountNbPredicates.java
@@ -0,0 +1,71 @@
+package Test;
+
+import PFSQGen.Queries;
+import PFSQGen.SparqlQueryParser;
+import org.apache.jena.graph.Node;
+import java.util.HashSet;
+import java.util.ArrayList;
+import org.apache.jena.query.Query;
+import org.apache.jena.query.QueryFactory;
+import org.apache.jena.sparql.core.TriplePath;
+import org.apache.jena.sparql.syntax.ElementPathBlock;
+import org.apache.jena.sparql.syntax.ElementVisitorBase;
+import org.apache.jena.sparql.syntax.ElementWalker;
+import java.util.Iterator;
+
+public class TestCountNbPredicates{
+    public static void main(String[] args) throws Exception {
+        
+        Queries queries1 = new Queries("queries/SWDF-CleanQueriesDecode.txt", "nonCrypte");
+        Queries queries2 = new Queries("queries/DBpedia3.5.1-CleanQueriesDecode.txt", "nonCrypte");
+        HashSet<Node> log1Predicates = new HashSet<Node>();
+        HashSet<Node> log2Predicates = new HashSet<Node>();
+        System.out.println("reading log1 ...");
+        for (SparqlQueryParser q1 : queries1.getQueries()) {
+            Query query1 = QueryFactory.create(q1.getQueryString());
+            if (query1 != null) {
+                ArrayList<Node> qPredicates = getPredicates(query1);
+                for (Node nlog1 : qPredicates) {  
+                    if ((nlog1 != null) && (!nlog1.toString().substring(0, 1).equals("?"))) {
+                        log1Predicates.add(nlog1);
+                    }
+                }
+            }
+        }
+        System.out.println("reading log2 ...");
+        for (SparqlQueryParser q2 : queries2.getQueries()) {
+            Query query2 = QueryFactory.create(q2.getQueryString());
+            if (query2 != null) {
+                ArrayList<Node> qPredicates = getPredicates(query2);
+                for (Node nlog1 : qPredicates) {  
+                    if ((nlog1 != null) && (!nlog1.toString().substring(0, 1).equals("?"))) {
+                        log2Predicates.add(nlog1);
+                    }
+                }
+            }
+        }
+        System.out.println("File 1 got " + log1Predicates.size() + " predicates");
+        System.out.println("File 2 got " + log2Predicates.size() + " predicates");
+    }
+    //From PFSQGen.PrunningLogsF
+    public static ArrayList<Node> getPredicates(Query q) {
+        ArrayList<Node> predicates = new ArrayList<Node>();
+        //System.out.println("q = "+q);                                                                                                                   
+        ElementWalker.walk(q.getQueryPattern(),
+                // For each element...                                                                                                                    
+            new ElementVisitorBase() {
+            // ...when it's a block of triples...                                                                                                         
+                public void visit(ElementPathBlock el) {
+                    // ...go through all the triples...                                                                                                       
+                    Iterator<TriplePath> triples = el.patternElts();
+                    while (triples.hasNext()) {
+                        // ...and grab the subject                                                                                                            
+                        predicates.add(triples.next().getPredicate());
+                    }
+                }
+            }
+        );
+
+        return predicates;
+    }
+}
